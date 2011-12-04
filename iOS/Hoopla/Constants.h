35f2a0ae96ef14ae872797a1183d15c3d78e4d5a
Chris Bruce <chrisabruce@gmail.com>
2011-12-03 19:35:01 -0800

    Testing sub init

---
+++ iOS/Hoopla/Constants.h
@@ -0,0 +1,10 @@
+//
+//  Constants.h
+//  Hoopla
+//
+//  Created by Chris Bruce on 12/3/11.
+//  Copyright (c) 2011 Diversion, Inc. All rights reserved.
+//
+
+#define API_URL @"api-hoopla.heroku.com"
+
