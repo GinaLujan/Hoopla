7099e468d48384c67918c1f3087e87d63cc10dd0
Chris Bruce <chrisabruce@gmail.com>
2011-12-26 12:00:11 -0800

    Added index to api.

--- Sinatra/api.rb
+++ Sinatra/api.rb
@@ -4,6 +4,15 @@ API_KEY_EVENTFUL = "psSPFxbbCzt73bHW"
 
 class API < Sinatra::Base
   
+  get "/" do
+    results = {:status => "ok",
+               :response_message => "Nothing at index",
+               :data => []}
+    content_type :json
+    results.to_json
+
+  end
+
   get "/recommendations/:id/:lat/:lng" do
     results = {
       :status => "ok",
